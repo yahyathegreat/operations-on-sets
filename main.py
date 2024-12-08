c4c15e0278ddf1e2aa67fab86c5a3b2a09083dbb
yahyathegreat <yahyamardan1234@gmail.com>
2024-12-08 14:50:33 +0500

    operations on sets

---
+++ main.py
@@ -0,0 +1,10 @@
+my_set = {1,2,3}
+print(my_set)
+my_set = {1.0, "HELLO", (1,2,3)}
+print(my_set)
+num_set = set([0, 1, 3, 4, 5])
+print("oringinal set:")
+print(num_set)
+num_set.pop()
+print("after removing the first element from the said set")
+print(num_set,"\n")
